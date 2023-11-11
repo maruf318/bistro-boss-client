328dc2446354b7bf489b97ff42f9de1e9bc33442
Maruf Hossain <hossainmaruf001@gmail.com>
2023-11-11 02:43:32 +0100

    menu page created and added banner and created custom hook to load all the menu data

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
 
 const Menu = () => {
   return (
@@ -6,7 +8,7 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h2>This is menu page</h2>
+      <Cover img={menuImg} title={"our menu"}></Cover>
     </div>
   );
 };
